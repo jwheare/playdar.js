09e692644042e490dbd18b4336ca2ee5bd4a7356
James Wheare <james@wheare.org>
2009-11-14 05:59:38 +0000

    The beginnings of a website. Features a hot inline code demo

---
+++ public/demo.js
@@ -0,0 +1,49 @@
+(function () {
+    function resolveForm () {
+        Playdar.client.resolve($('#demo input[name=artist]').val(), $('#demo input[name=track]').val());
+    }
+    Playdar.setupClient({
+        onStat: function (status) {
+            if (!status.authenticated) {
+                Playdar.client.start_auth();
+            }
+        },
+        onAuth: function () {
+            resolveForm();
+        },
+        onResults: function (response, finalAnswer) {
+            console.log('Polling ' + response.qid);
+            var id = 'results_qid_' + response.qid;
+            var row = $('#'+id);
+            if (!row[0]) {
+                row = $('<li id='+id+'>').text(response.query.artist + ' - ' + response.query.track);
+                $('#results').append(row);
+            }
+            row.append('.');
+            if (finalAnswer) {
+                if (response.solved) {
+                    row.css('background', 'green');
+                } else {
+                    row.css('background', 'orange');
+                }
+                if (!response.results.length) {
+                    row.css('background', 'red');
+                }
+                row.prepend('<span style="float:right;">'+response.results.length+'</span>');
+                console.dir(response);
+            }
+        }
+    });
+    var gone = false;
+    $('#demo').submit(function (e) {
+        e.preventDefault();
+        if (!gone) {
+            Playdar.client.go();
+            gone = true;
+        } else if (Playdar.client.is_authed()) {
+            resolveForm();
+        } else {
+            Playdar.client.start_auth();
+        }
+    });
+})();
